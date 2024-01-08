9649da6f237f45230ad602c94b6bf3b2edf27b6a
Jess purvis <jess.purvis@and.digital>
2024-01-08 10:43:53 +0000

    added missing dependency

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -4,15 +4,14 @@
 
 // for react-native only: @tamagui/config/v2-native
 
-import { config } from "@tamagui/config/v2";
-
-import { createTamagui } from "tamagui";
+import { config } from '@tamagui/config/v2';
+import { createTamagui } from 'tamagui';
 const tamaguiConfig = createTamagui(config);
 // this makes typescript properly type everything based on the config
 
 type Conf = typeof tamaguiConfig;
 
-declare module "tamagui" {
+declare module 'tamagui' {
   interface TamaguiCustomConfig extends Conf {}
 }
 export default tamaguiConfig;
